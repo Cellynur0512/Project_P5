99e773200714757e8dc007a51071cfe1ce79c6ca
Cellynur0512 <cellynurhaliza@smkwikrama.sch.id>
2023-09-14 11:44:49 +0700

    cel

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="landing">
+        <img src="img/wikrama-removebg-preview.png" alt="">
+        <p>SMK Wikrama Bogor</p>
+
+        <h1>Absensi Sholat Jumat</h1>
+        <h3>"Jumat Barokah, jangan lupa banyak berdoa, banyak sedekah dan banyak tersenyum."</h3>
+
+        <button>Login</button>
+    </div>
+</body>
+</html>
