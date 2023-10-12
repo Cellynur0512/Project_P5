90700b8775d147635991c58a298fe818c8e9a6c9
Cellynur0512 <cellynurhaliza@smkwikrama.sch.id>
2023-10-12 11:42:00 +0700

    celly

--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 </head>
 <body>
     <div class="landing">
-        <img src="img/wikrama-removebg-preview.png" alt="">
-        <p>SMK Wikrama Bogor</p>
-
-        <h1>Absensi Sholat Jumat</h1>
-        <h3>"Jumat Barokah, jangan lupa banyak berdoa, banyak sedekah dan banyak tersenyum."</h3>
-
-        <button>Login</button>
+        <img class="img-wk" src="img/wikrama-removebg-preview.png" alt="">
+        <p class="text-wk">SMK Wikrama Bogor</p>
+        <h1 class="text-slt"><span class="text-abs">Absensi</span> Sholat Jumat</h1>
+        <button class="btn-login"><b><a href="login.php">Login</a></b></button>
+        <pre class="text-hds">“Barangsiapa yang meninggalkan 
+shalat Jum’at tiga kali berturut-turut 
+maka ia telah melemparkan Islam ke 
+belakang punggungnya”</pre>
+        <img class="img-msj" src="img/masjid.png" alt="">
     </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hal_login">
+        <img class="img-wk" src="img/wikrama-removebg-preview.png" alt="">
+        <p class="text-wk">SMK Wikrama Bogor</p>
+        <h1 class="text-slt"><span class="text-abs">Absensi</span> Sholat Jumat</h1>
+        <pre class="text-hds2">“Barangsiapa yang meninggalkan 
+shalat Jum’at tiga kali berturut-turut 
+maka ia telah melemparkan Islam ke 
+belakang punggungnya”</pre>
+        <img class="img-msj2" src="img/masjid.png" alt="">
+        <div class="login">
+            <form action="" method="pos">
+                <h1 class="text-login"><b>Login</b></h1>
+                <input class="input" type="text" placeholder="Username"><br>
+                <input class="input" type="password" placeholder="Password"><br>
+                <input class="input-cek" type="checkbox">Login sebagai admin<br>
+                <input class="input-cek" type="checkbox">Login sebagai pembimbing<br>
+                <button class="btn-login2">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
